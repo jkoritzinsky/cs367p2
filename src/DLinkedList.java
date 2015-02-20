da12f46a78782d0f3068bbaa47036e42dddb5567
Jeremy Koritzinsky <koritzinsky@galapagos-20.cs.wisc.edu>
2015-02-19 18:48:35 -0600

    Added files from cs367 assignment directory.

---
+++ src/DLinkedList.java
@@ -0,0 +1,46 @@
+/**
+ * See ListADT for comments 
+ */
+public class DLinkedList<E> implements ListADT<E> {
+	
+
+	public DLinkedList(){
+	}
+
+
+	public void add(E item) {
+	}
+
+
+	public void add(int pos, E item) {
+	}
+
+
+	public boolean contains(E item) {
+		return false;
+	}
+
+
+	public E get(int pos) {
+		return null;
+	}
+
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+
+	public E remove(int pos) {
+		return null;
+	}
+
+
+	public int size() {
+		return 0;
+	}
+	
+	
+	
+
+}
